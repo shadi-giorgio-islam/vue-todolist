beea510bf4399e716f7be132acceeda443e2233b
Giorgio <shadigiorgioislam@gmail.com>
2021-01-28 15:30:54 +0100

    Add remove todo

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ var app = new Vue({
   data: {
     todos: ['HTML5', 'CSS', 'JQUERY', 'JAVASCRIPT', 'SQL', 'PHP'],
     todosDelete: []
+  },
+  methods: {
+    removeTodo(index){
+      this.todosDelete.push(this.todos[index]);
+      this.todos.splice(index, 1);
+    }
   }
 });
