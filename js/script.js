0ff707cfa7abe1313d7cdc3e712418f8acf2cea6
Giorgio <shadigiorgioislam@gmail.com>
2021-01-28 15:34:29 +0100

    Add restore todo

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ var app = new Vue({
     removeTodo(index){
       this.todosDelete.push(this.todos[index]);
       this.todos.splice(index, 1);
+    },
+    restoreTodo(index){
+      this.todos.push(this.todosDelete[index]);
+      this.todosDelete.splice(index, 1);
     }
   }
 });
