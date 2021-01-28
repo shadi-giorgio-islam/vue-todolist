3c568fa1053ae4e73030ae7797148b2883991c11
Giorgio <shadigiorgioislam@gmail.com>
2021-01-28 15:48:11 +0100

    Add remove all

--- js/script.js
+++ js/script.js
@@ -12,6 +12,13 @@ var app = new Vue({
     restoreTodo(index){
       this.todos.push(this.todosDelete[index]);
       this.todosDelete.splice(index, 1);
+    },
+    removeAll(){
+      const length = this.todos.length;
+      for (let i = 0; i < length; i++) {
+        this.todosDelete.push(this.todos[0]);
+        this.todos.splice(0, 1);
+      }
     }
   }
 });
